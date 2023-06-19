b76a0b0e4d8700a15861638760bd5f76aac423e4
Alice Troussier <alicedu05@hotmail.fr>
2023-06-19 17:39:51 +0200

    dernier_depot

--- eau04.rb
+++ eau04.rb
@@ -8,57 +8,44 @@
 
 #FONCTION
 def verifier_arg(arg)
-	if arg.match(/[a-zA-Z]/)
+	if arg.match(/[a-zA-Z]/) || arg.to_i < 0 || arg.empty?
 		return false
-		exit
 	end
 	return arg
 end
 
-def cree_liste(int) # amelioration (int..int*2).to_a
-	liste = []
-	fin_liste = int + int
-	range = (int + 1)..fin_liste
-	liste = range.to_a
-	return liste
-end
-
-def verifier_premier(liste)
-	est_premier = false
-	liste.each do |i|
-		p i
-	 while est_premier == false do
-	 	racine_chiffre = i**(1.0/2)
-	 	 (2..i).each do |j|
-	 	 	p j
-        if j % i == 0
-            est_premier = false
-            break
-        end
+def verifier_premier(num)
+	if num == 0 || num == 1
+		puts 2
+		exit
+	end	
+	chiffre = num + 1
+	est_premier = true
+	racine_chiffre = (chiffre)**(1.0/2)
+	(2..racine_chiffre).each do |i|
+		if chiffre % i == 0
+			est_premier = false
+			break
+		end
+	end	
+	if est_premier == true
+        return chiffre
+	else 
+		return verifier_premier(chiffre)
     end
-    if est_premier == true
-        puts "Oui,  est un nombre premier."
-    else
-        puts "Non, n’est pas un nombre premier."
-        break
-    end
-	 	end
-    end
-   return est_premier #booleen
 end
 
 #GESTION D'ERREUR
-puts -1 if ARGV.size != 1
-puts -1 unless verifier_arg(ARGV[0])
-#puts -1 if verifier_premier(liste) = false
+abort("-1") if ARGV.size != 1
+abort("-1") if verifier_arg(ARGV[0]) == false
+
 
 #PARSING
 input = ARGV[0]
 
 #RESOLUTION
 verifier_arg(input)
-liste_nombre = cree_liste(input.to_i)
-premier_supperieur = verifier_premier(liste_nombre)
+premier_supperieur = verifier_premier(input.to_f)
 
 #AFFICHAGE
-puts premier_supperieur
+puts premier_supperieur.to_i
